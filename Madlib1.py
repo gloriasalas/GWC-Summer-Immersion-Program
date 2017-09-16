6b1e97b290a4eed70715890f301ee8974bcf9e38
Gloria Salas <gjsalas131@gmail.com>
2017-09-16 15:30:51 -0700

    Add files via upload

---
+++ Madlib1.py
@@ -0,0 +1,31 @@
+adj_1 = input("Please type an adjective: ")
+obj_1 = input("Please type an object: ")
+pronoun_1 = input("Please type a pronoun: ")
+body_part_1 = input("Please type a body part: ")
+
+print("The trail of life persistently is " + adj_1 + " without a " + obj_1 + " to light my darkened way; instead " + pronoun_1 + " march, undaunted and unfazed, to follow paths that my " + body_part_1 + " surveys. ")
+
+pronoun_2 = input("Please type a pronoun: ")
+body_part_2 = input("Please type a body part: ")
+adj_2 = input("Please type an adjective: ")
+
+print("" + pronoun_2 + " know not where this drumming in my " + body_part_2 + " will lead, for I'm " + adj_2 + " of maps or charts.")
+
+adj_3 = input("Please type an adjective: ")
+noun_1 = input("Please type a noun: ")
+
+print("The only knowledge of this " + adj_3 + " quest I hold is where the " + noun_1 + " starts.")
+
+noun_2 = input("Please type a noun: ")
+noun_3 = input("Please type a noun: ")
+verb_1 = input("Please type a verb: ")
+noun_4 = input("Please type a noun: ")
+verb_2 = input("Please type a verb: ")
+
+print("The " + noun_2 + " claim my life is tossed away on " + noun_3 + " conforming simpletons and fools. If Father Time should " + verb_1 + " me as they say, I will freely " + noun_4 + " and " + verb_2 + " to his rule.")
+
+verb_3 = input("Please type a verb: ")
+noun_5 = input("Please type a noun: ")
+verb_4 = input("Please type a verb: ")
+
+print("So " + verb_3 + " me not, I will " + noun_5 + " at any cost; for those who " + verb_4 + " are not always lost.")
